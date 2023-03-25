7464f4b1a2d32b2568a355d0f4c90dff4688e5be
Bellusfennec <bellusfennec@gmail.com>
2023-03-25 22:15:32 +0300

    Добавылены в меню новые модули

--- src/menu.js
+++ src/menu.js
@@ -2,6 +2,9 @@ import { Menu } from './core/menu';
 import { BackgroundModule } from './modules/background.module';
 import { ShapeModule } from './modules/shape.module';
 import { ClicksModule } from './modules/clicks.module';
+import { TimerModule } from './modules/timer.module';
+import { SoundModule } from './modules/sound.module';
+import { MessageModule } from './modules/message.module';
 
 
 export class ContextMenu extends Menu {
@@ -11,6 +14,9 @@ export class ContextMenu extends Menu {
 		this.backgroundModule = new BackgroundModule('backgroundModule', 'Поменять цвет');
 		this.shapeModule = new ShapeModule('shapeModule', 'Создать фигуру');
 		this.clicksModule = new ClicksModule('clicksModule', 'Считать клики за 3 секунды');
+		this.timerModule = new TimerModule('timerModule', 'Включить таймер');
+		this.soundModule = new SoundModule('soundModule', 'Проверить звук');
+		this.messageModule = new MessageModule('messageModule', 'Отправить сообщение');
 	}
 
 	open() {
@@ -35,10 +41,16 @@ export class ContextMenu extends Menu {
 			const isBackgroundModule = (calledModule === 'backgroundModule');
 			const isShapeModule = (calledModule === 'shapeModule');
 			const isClicksModule = (calledModule === 'clicksModule');
+			const isTimerModule = (calledModule === 'timerModule');
+			const isSoundModule = (calledModule === 'soundModule');
+			const isMessageModule = (calledModule === 'messageModule');
 
 			if (isBackgroundModule) this.backgroundModule.trigger();
 			if (isShapeModule) this.shapeModule.trigger();
 			if (isClicksModule) this.clicksModule.trigger();
+			if (isTimerModule) this.timerModule.trigger();
+			if (isSoundModule) this.soundModule.trigger();
+			if (isMessageModule) this.messageModule.trigger();
 
 			this.close();
 		})
@@ -49,14 +61,23 @@ export class ContextMenu extends Menu {
 	}
 
 	add() {
-		const moduleHTML1 = this.backgroundModule.toHTML();
-		this.el.insertAdjacentHTML('beforeend', moduleHTML1);
+		const backgroundMenu = this.backgroundModule.toHTML();
+		this.el.insertAdjacentHTML('beforeend', backgroundMenu);
 
-		const moduleHTML2 = this.shapeModule.toHTML();
-		this.el.insertAdjacentHTML('beforeend', moduleHTML2);
+		const shapeMenu = this.shapeModule.toHTML();
+		this.el.insertAdjacentHTML('beforeend', shapeMenu);
 
-		const moduleHTML3 = this.clicksModule.toHTML();
-		this.el.insertAdjacentHTML('beforeend', moduleHTML3);
+		const clicksMenu = this.clicksModule.toHTML();
+		this.el.insertAdjacentHTML('beforeend', clicksMenu);
+
+		const timerMenu = this.timerModule.toHTML();
+		this.el.insertAdjacentHTML('beforeend', timerMenu);
+
+		const soundMenu = this.soundModule.toHTML();
+		this.el.insertAdjacentHTML('beforeend', soundMenu);
+
+		const messageMenu = this.messageModule.toHTML();
+		this.el.insertAdjacentHTML('beforeend', messageMenu);
 	}
 
 }
